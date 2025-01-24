d862427c1c253cddc968b16a72f9ce7194f1bf43
Kerby Keith Aquino <hello@kurojifusky.com>
2025-01-24 22:21:14 +0800

    note to self: use ts-node and/or nodemon for node typescript dev

--- scripts/check-react-lit-components.ts
+++ scripts/check-react-lit-components.ts
@@ -1,12 +1,17 @@
 /**
  * This script cross-checks if both React and Lit components match; otherwise, it'll automatically
- * fix the correct type. However, manual intervention is still necessary in case stuff breaks lol 
+ * fix the correct type. However, manual intervention is still necessary in case stuff breaks lol
  */
 import path from "path"
 import { glob } from "glob"
 
 const _cwd = process.cwd()
 
-const ignoredPaths = (...dirs: string[]) => {
+const mapPaths = (...dirs: string[]) => {
   return dirs.map((dir) => path.join(_cwd, dir))
 }
+
+const AB_CORE_DIR = "packages/auburn-core/src"
+const AB_CORE_COMPONENTS = mapPaths(...(await glob.glob(`${AB_CORE_DIR}/**/*.ts`, { ignore: `${AB_CORE_DIR}/vite-env.d.ts` })))
+
+console.log(AB_CORE_COMPONENTS)
